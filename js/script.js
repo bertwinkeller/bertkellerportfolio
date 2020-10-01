4aa4abbbb74c69006286240c0a6c25ef9783cc47
Bert Keller <bertwinkeller@gmail.com>
2020-10-01 11:14:54 -0700

    easy pie chart

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#slides').superslides({
         animation: 'fade',
         play: 5000,
@@ -6,11 +6,33 @@ $(document).ready(function(){
     })
 
     var typed = new Typed(".typed", {
-strings: ["","Web Developer", "React", "Node.js", "MongoDB", "Javascript", "HTML/CSS"],
-typeSpeed: 150, 
-loop: true,
-startDelay: 1000,
-showCursor: false
+        strings: ["", "Web Developer", "React", "Node.js", "MongoDB", "Javascript", "HTML/CSS"],
+        typeSpeed: 150,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    })
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+
+    $('.chart').easyPieChart({
+        //your options goes here
     })
 
 })
